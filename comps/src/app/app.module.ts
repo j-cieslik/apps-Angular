a44d6fcb3c56bd25f9ef57274728e349a8ad99a1
Kuba <jciesmail@gmail.com>
2020-12-28 19:17:45 +0100

    comps - Add TableComponent

--- comps/src/app/app.module.ts
+++ comps/src/app/app.module.ts
@@ -5,14 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { DividerComponent } from './shared/divider/divider.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NotFoundComponent,
-    DividerComponent
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
